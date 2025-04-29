2dc5df93c2a38935aa57610c3fd876c5385288d4
bladelee <xy_jiankang@163.com>
2025-04-29 14:12:51 +0800

    fix 资源日历 部门选择不弹出 bug

--- modules/people_resource/app/javascript/application2.js
+++ modules/people_resource/app/javascript/application2.js
@@ -1,6 +1,7 @@
 // 主应用 JS 文件，负责全局导入和配置
 import "@hotwired/turbo-rails"
-import "./controllers/index.js"
+// import "./controllers/index.js"
+import "controllers"
 import { Turbo } from "@hotwired/turbo-rails"
 
 Turbo.session.drive = false 
--- modules/people_resource/app/javascript/controllers/index.js
+++ modules/people_resource/app/javascript/controllers/index.js
@@ -1,8 +1,11 @@
 // Import and register all your controllers from the importmap under controllers/*
 
-import { application } from "./application"
-import DepartmentSelectorController from "./department_selector_controller.js"
-import ProjectSelectorController from "./project_selector_controller.js"
+// import { application } from "./application"
+// import DepartmentSelectorController from "./department_selector_controller.js"
+// import ProjectSelectorController from "./project_selector_controller.js"
+import { application } from "controllers/application"
+import DepartmentSelectorController from "controllers/department_selector_controller"
+import ProjectSelectorController from "controllers/project_selector_controller"
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
